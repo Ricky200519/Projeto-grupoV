be616080b9eff6027e9cbd575f5d287adf17bead
Ricardo <ricardof200519@gmai.com>
2025-11-05 01:27:47 +0000

    added navbar(template)

--- console/controllers/RbacController.php
+++ console/controllers/RbacController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\controllers;
+namespace console\controllers;
 
 use Yii;
 use yii\console\Controller;
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,6 +1,6 @@
 <?php
-
 /** @var \yii\web\View $this */
+
 /** @var string $content */
 
 use common\widgets\Alert;
@@ -9,76 +9,120 @@ use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
+use yii\helpers\Url;
 
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>" class="h-100">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+        <meta content="" name="keywords">
+        <meta content="" name="description">
 
-<header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-    }
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:wght@400;500;700;900&display=swap"
+              rel="stylesheet">
 
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-        'items' => $menuItems,
-    ]);
-    if (Yii::$app->user->isGuest) {
-        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-    } else {
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
-            )
-            . Html::endForm();
-    }
-    NavBar::end();
-    ?>
-</header>
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-<main role="main" class="flex-shrink-0">
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
+        <!-- Libraries Stylesheet -->
+        <link rel="stylesheet" href="lib/animate/animate.min.css"/>
+        <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-<footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
-    </div>
-</footer>
 
-<?php $this->endBody() ?>
-</body>
-</html>
+        <link href="../../web/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../../web/css/style.css" rel="stylesheet">
+
+    </head>
+    <body class="d-flex flex-column h-100">
+    <?php $this->beginBody() ?>
+
+    <header>
+
+        <?php
+        $currentAction = Yii::$app->controller->action->id;
+        ?>
+
+        <div class="container-fluid position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="<?= Url::toRoute(['site/index']) ?>" class="navbar-brand p-0">
+                    <h1 class="text-primary"><i class="fas fa-graduation-cap me-3"></i>LearnQuiz</h1>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="<?= Url::toRoute(['site/index']) ?>"
+                           class="nav-item nav-link <?= $currentAction == 'index' ? 'active' : '' ?>">Home</a>
+                        <a href="<?= Url::toRoute(['site/about']) ?>"
+                           class="nav-item nav-link <?= $currentAction == 'about' ? 'active' : '' ?>">About</a>
+                        <a href="service.html" class="nav-item nav-link">Services</a>
+                        <a href="<?= Url::toRoute(['site/contact']) ?>"
+                           class="nav-item nav-link <?= $currentAction == 'contact' ? 'active' : '' ?>">Contact Us</a>
+
+                        <?php if (Yii::$app->user->isGuest): ?>
+                            <a href="<?= Url::toRoute(['site/login']) ?>"
+                               class="nav-item nav-link <?= $currentAction == 'login' ? 'active' : '' ?>">
+                                <i class="fa fa-sign-in-alt text-primary me-2"></i>Login
+                            </a>
+                            <a href="<?= Url::toRoute(['site/signup']) ?>"
+                               class="nav-item nav-link <?= $currentAction == 'signup' ? 'active' : '' ?>">
+                                <i class="fa fa-user text-primary me-2"></i>Register
+                            </a>
+                        <?php else: ?>
+                            <span class="nav-item nav-link disabled fw-bold d-inline-flex align-items-center">
+                        <i class="fa fa-user text-primary me-2"></i>
+                        <?= Html::encode(Yii::$app->user->identity->username) ?>
+                    </span>
+
+                            <div class="nav-item">
+                                <?= Html::beginForm(['site/logout'], 'post', ['class' => 'd-inline']) ?>
+                                <button type="submit"
+                                        class="nav-item nav-link btn btn-link text-dark d-inline-flex align-items-center">
+                                    <i class="fa fa-sign-out-alt text-primary me-2"></i>Logout
+                                </button>
+                                <?= Html::endForm() ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+
+    </header>
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </main>
+
+    <footer class="footer mt-auto py-3 text-muted">
+        <div class="container">
+            <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+            <p class="float-end"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
+
+    <?php $this->endBody() ?>
+    <script src="../../web/js/main.js"></script>
+
+    </body>
+    </html>
 <?php $this->endPage();
